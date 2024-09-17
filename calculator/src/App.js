b0a0b1d915cd071eb741c369164acd3bbc27b6e5
CaValk4 <vitorcvk18@gmail.com>
2024-09-17 12:38:49 -0300

    container calculadora

---
+++ calculator/src/App.js
@@ -0,0 +1,15 @@
+import { Container, Content } from "./styles";
+import Input from './components/input';
+import Button from './components/button';
+
+const App = () => {
+  return (
+    <Container>
+    <Content>
+      <Input/><Button/>
+    </Content>
+    </Container>
+  );
+}
+
+export default App;
---
+++ calculator/src/components/input/styles.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    heigth: 75px;
+    background-color: #aaffaa;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+
+    input {
+        width: 100%;
+        heigth: 75px;
+        background-color: #aaffaa;
+        border: 0;
+        float: right;
+        clear: both;
+
+        font-size: 24px;
+        font-family: 'Roboto';
+    }
+`
+
+// part 2 - 05:50
